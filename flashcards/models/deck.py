f0645559d2d253d4c8c9791904a4cb826153af97
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-07 23:34:35 -0800

    feat: basic deck class (missing method logic)

---
+++ flashcards/models/deck.py
@@ -0,0 +1,20 @@
+class Deck:
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+        self.totalCards = 0
+
+        def addCard(card):
+            pass
+
+        def removeCard(card):
+            pass
+
+        def getCard(index):
+            pass
+        
+        def shuffleDeck():
+            pass
+
+        def getLength():
+            pass
