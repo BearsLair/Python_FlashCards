fea82ff06d7d6ff97b64bf92b6d28692e16c4e31
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-07 23:21:57 -0800

    feat: card class

---
+++ flashcards/models/card.py
@@ -0,0 +1,8 @@
+class Card:
+    def __init__(self, front, back, tags, id):
+        self.front = front
+        self.back = back
+        self.tags = tags
+        self.id = id
+
+c1 = Card("Dog", "Mammal", "animals, mammal", 1)
