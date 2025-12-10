cde7cdaea4e4dc0d8f4688aa4a7c703193928a67
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-12-09 20:33:06 -0800

    feat: Deck collection can be saved.

--- storage/json_store.py
+++ storage/json_store.py
@@ -1,5 +1,12 @@
-def save_deck(deck, filename):
-    pass
+import json
 
-def load_deck(filename):
-    pass
+def save_decks(decks):
+    with open("../data/json.data", "w") as data:
+        data.write(json.dumps(decks))
+    print("Saved as JSON to data folder.")
+
+def load_decks():
+    with open("../data/json.data", "r") as file:
+        data = json.load(file)
+    print("Read data in JSON file:")
+    print(data)
